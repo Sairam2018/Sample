149920c706cdba16268ba15c065373aba1e358e0
Sairam2018 <38127658+Sairam2018@users.noreply.github.com>
2018-06-20 16:06:37 +0530

    Update HomeController.cs

--- SampleApp/Controllers/HomeController.cs
+++ SampleApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace SampleApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.12345678";
+            ViewBag.Message = "Your contact page./123456789";
 
             return View();
         }
